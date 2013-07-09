ad289cb9b8a1db54eecb1f2bac32a9a7ff2e1989
Andreas Litt <andreas.litt@gmail.com>
2013-07-09 14:22:46 +0200

    Style improvements + pwstrength plugin. Not finished yet.

--- static/get.sh
+++ static/get.sh
@@ -3,3 +3,5 @@
 set -e
 
 wget http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
+wget http://code.jquery.com/jquery-2.0.3.min.js
+wget https://raw.github.com/ablanco/jquery.pwstrength.bootstrap/master/src/pwstrength.js
